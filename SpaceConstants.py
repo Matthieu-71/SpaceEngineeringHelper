b5167410dd1ef089bfed15703cf3658402424a95
Matthieu-71 <39201742+Matthieu-71@users.noreply.github.com>
2018-08-01 14:42:40 -0400

    Added SpaceConstants.py
    This file contains classes with important constants used in space engineering.

---
+++ SpaceConstants.py
@@ -0,0 +1,32 @@
+# Authour : Matthieu Durand
+# Creation date : 31 July 2018
+# Contains list of constants useful in space engineering
+
+# --- Standard gravtitional parameters of celestial bodies ---
+class mu:
+    Sun     = 132712000000
+    Mercury = 22030
+    Venus   = 324900
+    Earth   = 398600
+    Moon    = 4903
+    Mars    = 42828
+    Jupiter = 126686000
+    Saturn  = 37931000
+    Uranus  = 5794000
+    Neptune = 6835100
+    # Values are in km3 s-2, source Howard...
+
+# --- Mass of celestial bodies ---
+
+class mass:
+    Sun     = 1.989 * 10**30
+    Mercury = 330.2 * 10**21
+    Venus   = 4.869 * 10**24
+    Earth   = 5.974 * 10**24
+    Moon    = 73.48 * 10**21
+    Mars    = 641.9 * 10**21
+    Jupiter = 1.899 * 10**27
+    Saturn  = 568.5 * 10**24
+    Uranus  = 86.83 * 10**24
+    Neptune = 102.4 * 10**24
+    # Values are in kg, source Howard...
--- orbitCalculations.py
+++ orbitCalculations.py
@@ -2,23 +2,39 @@ import numpy as np
 from numpy import linalg as la
 import math as m
 import AnomalyConverter as ac
+import SpaceConstants as sc
 import imp
 import matplotlib.pyplot as pl
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
 
 
 # Authour : Matthieu Durand
 # Creator date : July 30 2018
 # Script to calculate perital position using various methods
 
+def semiLat(a,e):
+        # Calculates the semi-latus rectum in kilometers
+        # a - semimajor axis, e - eccentricity
+        p = a*(1 - e**2)
+        return p
+
+def speRAM(p,body):
+        # Calculates the specific angular momentum of a satellite about a body
+        # p - semilatus rectum [km], body - name as a string
+        h = m.sqrt(getattr(sc.mu,body)*p)
+        return h
+
 def stateVector(a,ec,inc,RA,Ap,TA):
-    # This functions returns the state vectors (radius and velocity) given six perital elements
+    # This functions returns the state vectors (radius and velocity) given six orbital elements
     # Semi-major axis, eccentricity, inclination, RAAN, argument of perigee, true anomaly
 	
-	p = a*(1 - ec**2) # Semi-latus rectum in km
-	h = m.sqrt(mu_Earth*p) # Specific relative angular momentum, in km2 s-1
+	p = semiLat(a,ec) # Semi-latus rectum in km
+	#h = m.sqrt(mu_Earth*p) # Specific relative angular momentum, in km2 s-1
+	h = speRAM(p,'Earth')
 	radius = a*(1 - ec**2)/(1 + ec*m.cos(TA)) # Radius in kilometres, found with true anomaly
 	
-	perR = (h**2/mu_Earth)*(1/(1 + ec**m.cos(TA)))*np.array([m.cos(TA),m.sin(TA),0]) # Compute radius in perifocal frame
+	perR = (h**2/mu_Earth)*(1/(1 + ec*m.cos(TA)))*np.array([m.cos(TA),m.sin(TA),0]) # Compute radius in perifocal frame
 	geoR = np.array([perR[0]*(m.cos(Ap)*m.cos(RA)-m.sin(Ap)*m.cos(inc)*m.sin(RA)) - perR[1]*(m.sin(Ap)*m.cos(RA)+m.cos(Ap)*m.cos(inc)*m.sin(RA)),
 					 perR[0]*(m.cos(Ap)*m.sin(RA)-m.sin(Ap)*m.cos(inc)*m.cos(RA)) + perR[1]*(m.cos(Ap)*m.cos(inc)*m.cos(RA)-m.sin(Ap)*m.sin(RA)),
 					 perR[0]*(m.sin(Ap)*m.sin(inc))+perR[1]*(m.cos(Ap)*m.sin(inc))]) # Compute radius in geocentric frame
@@ -28,27 +44,32 @@ def stateVector(a,ec,inc,RA,Ap,TA):
 					 perV[0]*(m.cos(Ap)*m.sin(RA)-m.sin(Ap)*m.cos(inc)*m.cos(RA)) + perV[1]*(m.cos(Ap)*m.cos(inc)*m.cos(RA)-m.sin(Ap)*m.sin(RA)),
 					 perV[0]*(m.sin(Ap)*m.sin(inc))+perV[1]*(m.cos(Ap)*m.sin(inc))]) # Compute radius in geocentric frame
 					 
-	return perR
+	return geoR
 	
 
-mu_Earth = 3.986004418*(10**5) # Earth's standard gravitational parameter, in km3 s-2
+mu_Earth = sc.mu.Earth # Earth's standard gravitational parameter, in km3 s-2
 
-sma = 42000 # In km
-ecc = 0.01
-inc = 0
-raa = 0
-aop = 0
+sma = 10000 # In km
+ecc = np.random.random()
+inc = np.random.random()*2*np.pi
+raa = np.random.random()*2*np.pi
+aop = np.random.random()*2*np.pi
 tra = np.linspace(0, 2*np.pi, num=100) # In radians
+print("Orbit generated for: e = " + str(ecc) + ", i = " + str(inc*180/np.pi) + " deg, RAAN = " + str(raa*180/np.pi) + " deg, AoP = " + str(aop*180/np.pi) + " deg.")
 
 i = 0
 X = np.zeros(100)
 Y = np.zeros(100)
-for i in range(0,99):
-	temp = stateVector(sma,ecc,inc,raa,aop,tra[i])
-	X[i] = temp[0]
-	Y[i] = temp[1]
-	
-pl.plot(X,Y)
+Z = np.zeros(100)
+for i in range(0,100):
+        temp = stateVector(sma,ecc,inc,raa,aop,tra[i])
+        X[i] = temp[0]
+        Y[i] = temp[1]
+        Z[i] = temp[2]
+
+fig = pl.figure()
+ax = fig.gca(projection='3d')
+pl.plot(X,Y,Z, color='k')
 pl.show()
 # stateVector(sma,ecc,inc,raa,aop,tra)
 # new = ac.Mean2Ecce(0.488692,ecc)
